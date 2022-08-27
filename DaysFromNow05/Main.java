c31604673b79b1a18d113448715fe53284d1aeaa
James <entreur@gmail.com>
2022-08-27 14:07:41 +0400

    Challenge 05

---
+++ DaysFromNow05/Main.java
@@ -0,0 +1,19 @@
+import java.time.temporal.TemporalAdjuster;
+import java.util.Calendar;
+import java.util.Date;
+
+class Program{
+
+    static Calendar cal = Calendar.getInstance();
+
+    public static void daysFromToday(int days){
+        
+        cal.add(Calendar.DATE, days);
+        Date daysFromNow = cal.getTime();
+        System.out.println("Hundred days from today will be " + daysFromNow);
+    }
+
+    public static void main(String[] args) {
+        daysFromToday(100);
+    }
+}
